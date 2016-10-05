036b9a32f5e87326070413d96632e6b4cbc69fe0
Henry Chipman <hdchipman@msn.com>
2016-10-04 21:28:59 -0700

    refactored script and added texting service, still need to add in mailing

--- choresgen.py
+++ choresgen.py
@@ -1,92 +1,87 @@
-# Chore script
+# Henry Chipman
+# Chorescript 1.2.0
+# 10-4-2016
+
 import datetime
-from datetime import date
-import requests
-import smtplib
+from twilio.rest import TwilioRestClient
 
-today = list(str(datetime.date.today()))
-today_s = str(today[5] + today[6] + '/' + today[8] + today[9])
+brajs = {
+  'Kelly': {
+      'phone':'+12068192190',
+      'email':'bkpratt@uw.edu',
+      'chore':''
+    },
+  'Nicky': {
+      'phone':'+12063590744',
+      'email':'nick.garzon@gmail.com',
+      'chore':''
+    },
+  'Jake': {
+      'phone':'+15092209113',
+      'email':'jwmouser@gmail.com',
+      'chore':''
+    },
+  'Torin': {
+      'phone':'+12063309704',
+      'email':'torin206@gmail.com',
+      'chore':''
+    },
+  'Jeremy': {
+      'phone':'+12062912466',
+      'email':'perezje43@gmail.com',
+      'chore':''
+    },
+  'Chip': {
+      'phone':'+12062406461',
+      'email':'hdchipman@msn.com',
+      'chore':''
+    }
+}
 
-now = datetime.date.today()
-today_date = date(now.year, now.month, now.day)
-start_date = date(2016, 9, 12)
-delta = today_date - start_date
+chores = [
+  'Take out full trashes in the house as needed and to curb on Tuesday',
+  'Sweep and Clean kitchen/dining room/living room floor on Weekend',
+  'Clean bathroom (floor/sink/toilet) (tub if dirty) on Weekend',
+  'Organize kitchen (Dishes/stovetop/counters) on Weekend',
+  'Groundskeeping (clean trash/mow/clean porch) on Weekend',
+  'Bye week'
+]
 
+### Methods ###################################################################### 
 
-def send_email(user, pwd, recipient, subject, body):
+# assigns chores to people based on the week
+def assign_chores():
+  weeks = calculate_weeks()
+  for index, braj in enumerate(brajs):
+    chore_index = (weeks + index) % len(chores)
+    brajs[braj]['chore'] = chores[chore_index]
 
-    gmail_user = user
-    gmail_pwd = pwd
-    FROM = user
-    TO = recipient if type(recipient) is list else [recipient]
-    SUBJECT = subject
-    TEXT = body
+# returns number of weeks passed since the first day of 2016
+def calculate_weeks():
+  today = datetime.date.today()
+  someday = datetime.date(2016, 1, 1)
+  diff = today - someday
+  weeks = int(diff.days / 7)
+  return weeks
 
-    # Prepare actual message
-    message = """\From: %s\nTo: %s\nSubject: %s\n\n%s
-    """ % (FROM, ", ".join(TO), SUBJECT, TEXT)
-    try:
-        server = smtplib.SMTP("smtp.gmail.com", 587)
-        server.ehlo()
-        server.starttls()
-        server.login(gmail_user, gmail_pwd)
-        server.sendmail(FROM, TO, message)
-        server.close()
-        print 'successfully sent the mail'
-    except:
-        print "failed to send mail"
+# iterates through all brajs and texts them their chore
+def send_texts():
+  ACCOUNT_SID = "ACa6b4755c8f58071c638a0b5f775e90e9" 
+  AUTH_TOKEN = "0a64c4459d018e93c909b67290a31a95" 
+  client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
+  for braj in brajs:
+    text = "BRAJ HOUSE MESSAGING\n" + braj + "\nYour chore for this week: " + brajs[braj]['chore'] 
+    number = brajs[braj]['phone']
+    # client.messages.create(from_='+12062020160',
+    #                      to=number,
+    #                      body=text)
 
+# TODO email out list
+def send_mail():
+  print('yo')
 
+### Execute ########################################################################
 
-## Main_______________________________________________________________________
-#_____________________________________________________________________________
-
-ppl = ['Kelly','Nicky','Jake','Torin','Jeremy','Chip']
-chores = ['Take out full trashes in the house as needed and to curb on Tuesday',\
-          'Sweep and Clean kitchen/dining room/living room floor on Weekend',\
-          'Clean bathroom (floor/sink/toilet) (tub if dirty) on Weekend',\
-          'Organize kitchen (Dishes/stovetop/counters) on Weekend',\
-          'Groundskeeping (clean trash/mow/clean porch) on Weekend',\
-          'Bye week']
-
-emails = ['foo@bar.com','bar@foo.com']
-
-sub = 'Chore assignments for the week of %s' % today_s
-
-
-if delta.days > 42:
-    while delta.days > 42:
-        delta.days -= 42
-    print delta.days
-
-
-# This works but it needs some desperate cleanup
-if (delta.days == 42):
-    body = '%s: %s\n%s: %s\n%s: %s\n%s: %s\n%s: %s\n%s: %s' % \
-              (ppl[1],chores[0],ppl[2],chores[1],ppl[3],chores[2],ppl[4],\
-              chores[3],ppl[5],chores[4],ppl[0],chores[5])
-elif (delta.days == 35):
-    body = '%s: %s\n%s: %s\n%s: %s\n%s: %s\n%s: %s\n%s: %s' % \
-              (ppl[2],chores[0],ppl[3],chores[1],ppl[4],chores[2],ppl[5],\
-              chores[3],ppl[0],chores[4],ppl[1],chores[5])
-elif (delta.days == 28):
-    body = '%s: %s\n%s: %s\n%s: %s\n%s: %s\n%s: %s\n%s: %s' % \
-              (ppl[3],chores[0],ppl[4],chores[1],ppl[5],chores[2],ppl[0],\
-              chores[3],ppl[1],chores[4],ppl[2],chores[5])
-elif (delta.days == 21):
-    body = '%s: %s\n%s: %s\n%s: %s\n%s: %s\n%s: %s\n%s: %s' % \
-              (ppl[4],chores[0],ppl[5],chores[1],ppl[0],chores[2],ppl[1],\
-              chores[3],ppl[2],chores[4],ppl[3],chores[5])
-
-elif (delta.days == 14):
-    body = '%s: %s\n%s: %s\n%s: %s\n%s: %s\n%s: %s\n%s: %s' % \
-              (ppl[5],chores[0],ppl[0],chores[1],ppl[1],chores[2],ppl[2],\
-              chores[3],ppl[3],chores[4],ppl[4],chores[5])
-
-elif (delta.days == 7):
-    body = '%s: %s\n%s: %s\n%s: %s\n%s: %s\n%s: %s\n%s: %s' % \
-              (ppl[0],chores[0],ppl[1],chores[1],ppl[2],chores[2],ppl[3],\
-              chores[3],ppl[4],chores[4],ppl[5],chores[5])
-
-
-send_email(YOUR_EMAIL, YOUR_EMAIL_KEY, emails, sub, body)
+assign_chores() # assign everyone chores
+send_texts() # send texts
+# send_mail() # send emails
